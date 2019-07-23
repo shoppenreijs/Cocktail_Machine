c926cd6052f0acdf72dd7e9c4c655c0f09e83395
Stan <s.p.w.m.hoppenreijs@student.tue.nl>
2019-07-23 20:56:32 +0200

    Update relay.py

--- relay.py
+++ relay.py
@@ -6,32 +6,29 @@ motor2 = 20
 motor3 = 16
 motor4 = 26
 
+motor_list = [motor1, motor2, motor3, motor4]
 # GPIO setup
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(motor1, GPIO.OUT)
-GPIO.setup(motor2, GPIO.OUT)
-GPIO.setup(motor3, GPIO.OUT)
-GPIO.setup(motor4, GPIO.OUT)
+GPIO.setup(motor_list, GPIO.OUT)
 
 
 def motor_on(pin):
-    GPIO.output(pin, GPIO.HIGH)  # Turn motor on
+    GPIO.output(pin, GPIO.LOW)  # Turn motor on
 
 
 def motor_off(pin):
-    GPIO.output(pin, GPIO.LOW)  # Turn motor off
+    GPIO.output(pin, GPIO.HIGH)  # Turn motor off
 
 
-if __name__ == '__main__':
-    try:
-        motor_on(motor1)
-        time.sleep(2)
-        motor_off(motor1)
-        time.sleep(2)
-        motor_on(motor2)
-        time.sleep(1)
-        motor_off(motor2)
-        time.sleep(1)
-        GPIO.cleanup()
-    except KeyboardInterrupt:
-        GPIO.cleanup()
+try:
+    motor_on(motor1)
+    time.sleep(2)
+    motor_off(motor1)
+    time.sleep(2)
+    motor_on(motor2)
+    time.sleep(1)
+    motor_off(motor2)
+    time.sleep(1)
+    GPIO.cleanup()
+except KeyboardInterrupt:
+    GPIO.cleanup()
