8a7bc38445f40802d1a004625384324c0f1dbb2c
Stan <s.p.w.m.hoppenreijs@student.tue.nl>
2019-07-23 21:51:25 +0200

    Update relay.py

--- relay.py
+++ relay.py
@@ -13,25 +13,25 @@ GPIO.setup(motor_list, GPIO.OUT)
 
 
 def motor_on(pin):
-    GPIO.output(pin, GPIO.HIGH)  # Turn motor on
+    GPIO.output(pin, GPIO.LOW)  # Turn motor on
 
 
 def motor_off(pin):
-    GPIO.output(pin, GPIO.LOW)  # Turn motor off
+    GPIO.output(pin, GPIO.HIGH)  # Turn motor off
 
 
 try:
     motor_on(motor1)
-    print('motor1 high')
+    print('motor1 on')
     time.sleep(2)
     motor_off(motor1)
-    print('motor1 low')
+    print('motor1 off')
     time.sleep(2)
     motor_on(motor2)
-    print('motor2 high')
+    print('motor2 on')
     time.sleep(1)
     motor_off(motor2)
-    print('motor2 low')
+    print('motor2 off')
     time.sleep(1)
     GPIO.cleanup()
 except KeyboardInterrupt:
