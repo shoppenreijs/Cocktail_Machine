987c795ac057665f985df74c36e2bcb66347f07d
Stan <s.p.w.m.hoppenreijs@student.tue.nl>
2019-07-29 22:01:13 +0200

    Update test_Pumps.py

--- test_Pumps.py
+++ test_Pumps.py
@@ -14,6 +14,10 @@ pump_ch = [21, 20, 16, 26]
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pump_ch, GPIO.OUT, initial=GPIO.HIGH)
 
+GPIO.output(pump_ch[0], GPIO.LOW)
+time.sleep(10)
+GPIO.output(pump_ch[0], GPIO.HIGH)
+
 GPIO.output(pump_ch[1], GPIO.LOW)
 time.sleep(10)
 GPIO.output(pump_ch[1], GPIO.HIGH)
