aa2607e3213e0c4185fc97976fee589d1891851f
Stan <s.p.w.m.hoppenreijs@student.tue.nl>
2019-07-29 21:56:25 +0200

    Create test_Pumps.py

---
+++ test_Pumps.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 29 21:53:02 2019
+
+@author: Stan
+"""
+
+import RPi.GPIO as GPIO
+import time
+
+pump_ch = [21, 20, 16, 26]
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pump_ch, GPIO.OUT, initial=GPIO.HIGH)
+
+GPIO.output(pump_ch[1], GPIO.LOW)
+time.sleep(2)
+GPIO.output(pump_ch[1], GPIO.HIGH)
+
+GPIO.output(pump_ch[2], GPIO.LOW)
+time.sleep(2)
+GPIO.output(pump_ch[2], GPIO.HIGH)
+
+GPIO.output(pump_ch[3], GPIO.LOW)
+time.sleep(2)
+GPIO.output(pump_ch[3], GPIO.HIGH)
+
+GPIO.cleanup()
