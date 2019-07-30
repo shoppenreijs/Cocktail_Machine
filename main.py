905806aadf99cc8dbe11d45ea846ccfec861af0c
Stan <s.p.w.m.hoppenreijs@student.tue.nl>
2019-07-30 19:30:42 +0200

    test nieuw

--- main.py
+++ main.py
@@ -11,31 +11,34 @@ import time
 import sys
 
 sys.path.insert(0, '/home/pi/Cocktail_Machine/mylibs')                                
-from relay import PumpRelay
+#from relay import PumpRelay
+from pumps import Pumps
 #from HC_SR04 import Stock_Sensor
 #from cocktail_gen import Cocktail_Generator
 
 #cocktail = Cocktail_Generator( cocktail_name = 'mix1', volume = 0.1 )
 pump_ch = [21, 20, 16, 26]                              #Relay channels
 HC_SR04_ch = [[23, 24], [17, 18], [27,22], [13,19]]     #[TRIG, ECHO] sensor pair
-pumps = PumpRelay( pump_ch )
+#pumps = PumpRelay( pump_ch )
 #stock_amount = Stock_Sensor( HC_SR04_ch )
 
+pump1 = Pumps( pump_ch[1] )
+
 ## Test pumps
-pumps.motor_on( pump_ch[1] )
+pump1.on()
 time.sleep(4)
-pumps.motor_off( pump_ch[1] )
+pump1.off()
 
-pumps.motor_on( pump_ch[2] )
-time.sleep(4)
-pumps.motor_off( pump_ch[2] )
-
-pumps.motor_on( pump_ch[3] )
-time.sleep(4)
-pumps.motor_off( pump_ch[3] )
-
-pumps.motor_on( pump_ch[4] )
-time.sleep(4)
-pumps.motor_off( pump_ch[4] )
+#pumps.motor_on( pump_ch[2] )
+#time.sleep(4)
+#pumps.motor_off( pump_ch[2] )
+#
+#pumps.motor_on( pump_ch[3] )
+#time.sleep(4)
+#pumps.motor_off( pump_ch[3] )
+#
+#pumps.motor_on( pump_ch[4] )
+#time.sleep(4)
+#pumps.motor_off( pump_ch[4] )
 
 GPIO.cleanup() 
---
+++ mylibs/pumps.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 30 19:25:42 2019
+
+@author: Stan
+"""
+
+import RPi.GPIO as GPIO
+
+class Pumps:
+    
+    def __init__ ( self, pump_ch ):
+    
+        self.pin = pump_ch
+        GPIO.setmode( GPIO.BCM )
+        GPIO.setup( pump_ch, GPIO.OUT, initial=GPIO.HIGH )
+    
+    
+    def on( self ):
+        GPIO.output( self.pin, GPIO.LOW)  # Turn motor on
+    
+    
+    def off( self ):
+        GPIO.output( self.pin, GPIO.HIGH)  # Turn motor off
+    
+    
+
