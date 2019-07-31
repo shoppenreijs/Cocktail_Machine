11aad984dc3d907a20d444726fb8857290ff1ec2
Stan <s.p.w.m.hoppenreijs@student.tue.nl>
2019-07-31 23:01:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -70,34 +70,34 @@ def brew_cocktail ( cocktail_name, volume=100 ):
     pump_times = cocktail.make_cocktail()
     
     #for i in range(len(pump_times)):
-            if pump_times[0] != 0 and  pump_times[1] != 0:
-                pumps[0].on()
-                pumps[1].on()
-                time.sleep( pump_times[1] )
-                pumps[1].off()
-                time.sleep( (pump_times[0]-pump_times[1] )   #self.pumptimes[i])
-                pumps[0].off()
+    if pump_times[0] != 0 and  pump_times[1] != 0:
+        pumps[0].on()
+        pumps[1].on()
+        time.sleep( pump_times[1] )
+        pumps[1].off()
+        time.sleep( pump_times[0]-pump_times[1])   #self.pumptimes[i])
+        pumps[0].off()
                 
-            if pump_times[0] != 0 and  pump_times[2] != 0:
-                pumps[0].on()
-                pumps[2].on()
-                time.sleep( pump_times[2] )
-                pumps[2].off()
-                time.sleep( pump_times[0] - pump_times[2] )   #self.pumptimes[i])
-                pumps[0].off()
-            if pump_times[4] != 0 and  pump_times[2] != 0:
-                pumps[4].on()
-                pumps[2].on()
-                time.sleep( pump_times[2] )
-                pumps[2].off()
-                time.sleep( pump_times[4] - pump_times[2] )   #self.pumptimes[i])
-                pumps[4].off()
-             if pump_times[2] != 0 and  pump_times[3] == 0 and pump_times[1] == 0 and pump_times[0] == 0:
+    if pump_times[0] != 0 and  pump_times[2] != 0:
+        pumps[0].on()
+        pumps[2].on()
+        time.sleep( pump_times[2] )
+        pumps[2].off()
+        time.sleep( pump_times[0] - pump_times[2] )   #self.pumptimes[i])
+        pumps[0].off()
+    if pump_times[4] != 0 and  pump_times[2] != 0:
+        pumps[4].on()
+        pumps[2].on()
+        time.sleep( pump_times[2] )
+        pumps[2].off()
+        time.sleep( pump_times[4] - pump_times[2] )   #self.pumptimes[i])
+        pumps[4].off()
+    if pump_times[2] != 0 and  pump_times[3] == 0 and pump_times[1] == 0 and pump_times[0] == 0:
                 
-                pumps[2].on()
-                time.sleep( pump_times[2] )
-                pumps[2].off()
-                  
+        pumps[2].on()
+        time.sleep( pump_times[2] )
+        pumps[2].off()
+    
                 
               
                 
